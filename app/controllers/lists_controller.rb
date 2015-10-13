c6e970c379e7e64445e5e8b39d84903c02602b42
Janice Dunn <janicedunn53@gmail.com>
2015-10-12 19:18:31 -0700

    DRY code in lists controller.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,14 @@
 class ListsController < ApplicationController
   def index
     @lists = List.all
-    render :index
   end
 
   def show
     @list = List.find(params[:id])
-    render :show
   end
 
   def new
     @list = List.new
-    render :new
   end
 
   def create
